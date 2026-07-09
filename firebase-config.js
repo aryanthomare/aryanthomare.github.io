8b83026eb38c53e1d993ff53593d6a31f3ab7c75
Aryan <aryanthomare@gmail.com>
2026-07-09 01:35:23 -0400

    Fixed to use firebase

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,6 @@
 // enforced by Firestore security rules (public read, single-user write).
 // Find them in Firebase console > Project settings > General > Your apps.
 window.FIREBASE_CONFIG = {
-    projectId: 'YOUR_FIREBASE_PROJECT_ID',
-    apiKey: 'YOUR_FIREBASE_WEB_API_KEY'
+    projectId: "portfoliosite-d967c",
+    apiKey:  "AIzaSyAAJ2Gu-r2mQ3oOAMgmWId5qqXMcLaZ2_4"
 };
